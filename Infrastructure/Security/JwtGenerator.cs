da8d32be39cfb42d9d5f6ceb3ddfabaaf0fac3d0
Daniel Beeston <dbwhite41@hotmail.com>
2020-04-05 16:04:09 +1000

    end of section 12

---
+++ Infrastructure/Security/JwtGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Application.Interfaces;
+using Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Security
+{
+    public class JwtGenerator : IJwtGenerator
+    {
+        private readonly SymmetricSecurityKey _key;
+        public JwtGenerator(IConfiguration config)
+        {// generate signing credentials (allows our API to trust the token coming from the client)
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+            
+            // create credentials from our key with secure algorithm. 
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7), // not great from a security point of view. This should be as low as possible
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAcessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAcessor)
+        {
+            _httpContextAcessor = httpContextAcessor;
+        }
+
+        public string GetCurrentUsername()
+        {
+            var username = _httpContextAcessor.HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return username;
+        }
+    }
+}
