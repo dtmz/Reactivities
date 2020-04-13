504c08ed3ab4245bffee2afe4ea48199a2f258d8
Daniel Beeston <dbwhite41@hotmail.com>
2020-04-13 14:06:15 +1000

    end of section 14

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using AutoMapper;
+using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -11,20 +12,22 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
+        public class Query : IRequest<List<ActivityDto>> { }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, List<ActivityDto>>
         {
             private readonly DataContext _context;
             private readonly ILogger<List> _logger;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context, ILogger<List> logger, IMapper mapper)
             {
                 _context = context;
                 _logger = logger;
+                _mapper = mapper;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 //try
                 //{
@@ -42,7 +45,7 @@ namespace Application.Activities
 
                 var activities = await _context.Activities.ToListAsync(cancellationToken);
 
-                return activities;
+                return _mapper.Map<List<Activity>, List<ActivityDto>>(activities);
             }
         }
     }
